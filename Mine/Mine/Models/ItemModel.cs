3e86e860da2338db6ee00ec9e0c307c5452e167d
messnerd <messnera@seattleu.edu>
2021-01-25 19:40:15 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,18 @@
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Items for the Characters and Monsters to use
+    /// </summary>
     public class ItemModel
     {
+        // The ID for the Item
         public string Id { get; set; }
+
+        // The Display text for the Item
         public string Text { get; set; }
+
+        // The Description of the Item
         public string Description { get; set; }
 
         // The Value of the Item
